3d230aea01f05e79d4191ca4bd8e940ba300b3bb
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-27 10:16:00 +0200

    Implemented AuthenticationManagerBuilder

--- src/main/java/zw/co/afrosoft/configs/MySecurityConfig.java
+++ src/main/java/zw/co/afrosoft/configs/MySecurityConfig.java
@@ -1,8 +1,14 @@
 package zw.co.afrosoft.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,9 +22,24 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/myCards").authenticated()
                 .antMatchers("/notifications").permitAll()
                 .antMatchers("/contact").permitAll()
-                .antMatchers("/welcome").permitAll()
                 .and()
                 .formLogin().and()
                 .httpBasic();
+    }
+
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("admin").password("1234").authorities("admin").and().
+//                withUser("user").password("12345").authorities("read").and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("admin").password("1234").authorities("admin").build();
+        UserDetails user1 = User.withUsername("user").password("1234").authorities("read").build();
+        userDetailsService.createUser(user);
+        userDetailsService.createUser(user1);
+        auth.userDetailsService(userDetailsService);
     }
 }
